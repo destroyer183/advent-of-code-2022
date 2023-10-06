5eab3a7c91db21e9092c5c7c4dc0d324131e4b04
destroyer183 <jzante99@gmail.com>
2023-10-06 12:26:26 -0400

    finished day 8 part 1

--- Day 8/Day 8 Code.py
+++ Day 8/Day 8 Code.py
@@ -35,7 +35,7 @@ class Forest:
 
 class Tree:
 
-    def __init__(self, rownum, columnnum, height: int, visible = None) -> None:
+    def __init__(self, rownum, columnnum, height: int, visible = False) -> None:
 
         self.rownum = rownum
         self.columnnum = columnnum
@@ -60,16 +60,154 @@ with open("input.txt", "r") as reader:
         
             y.append(Tree(Forest.row_total, i, x[i]))
         
-        Forest('row', x)
+        Forest('row', y)
+
+print('')
 
 
+for key in Forest.forest:
+
+    temp = []
+
+    for val in Forest.forest[key]:
+
+        temp.append(val.height)
+
+    print(f"row: {('').join(temp)}")
+
+
+
+x = []
 
 for i in range(len(Forest.forest)):
 
-    x = [x[i] for x in Forest.forest.values()]
+    x.append([x[i] for x in Forest.forest.values()])
 
-    Forest('column', x)
+
+
+for e in x:
+
+    Forest('column', e)
+
+
+print('')
+
+
+
+# for key in Forest.forest:
+
+#     temp = []
+
+#     if key.type == 'column':
+
+#         for val in Forest.forest[key]:
+
+#             temp.append(val.height)
+
+#     print(f"column: {('').join(temp)}")
 
 
 
 # the whole dict is now correctly filled, next, whether each tree is visible or not needs to be computed.
+
+count1 = 0
+
+
+for key in Forest.forest:
+
+    previous = None
+
+    for a in range(len(Forest.forest[key])):
+
+        if a == 0:
+
+            Forest.forest[key][a].visible = True
+
+            previous = Forest.forest[key][a].height
+
+
+
+
+        elif Forest.forest[key][a].height > previous:
+
+            Forest.forest[key][a].visible = True
+
+            previous = Forest.forest[key][a].height
+
+
+
+
+    previous = None
+
+
+
+    for b in range(len(Forest.forest[key]) - 1, -1, -1):
+
+        if b == len(Forest.forest[key]) - 1:
+
+            Forest.forest[key][b].visible = True
+
+            previous = Forest.forest[key][b].height
+
+
+
+
+        elif Forest.forest[key][b].height > previous:
+
+            Forest.forest[key][b].visible = True
+
+            previous = Forest.forest[key][b].height
+
+
+
+
+count = 0
+
+copy = Forest.forest.copy()
+
+for key in copy:
+
+    if key.type == 'row':
+
+        for val in copy[key]:
+
+            if val.visible:
+
+                count += 1
+
+
+print(f"number of visible trees: {count}")
+
+
+
+
+# create a bunch of lists containing either 1 or 0 to represent each tree
+
+count2 = 0
+
+for key in Forest.forest:
+
+    temp = []
+
+    if key.type == 'row':
+
+        for val in Forest.forest[key]:
+
+            # print(val)
+
+            if val.visible:
+
+                temp.append('1')
+
+                count2 += 1
+
+            else:
+
+                temp.append('0')
+
+        print(f"row: {('').join(temp)}")
+
+print(count2)
+
+
+# look at line 99, number 7. in that column, while the 0 above the 2 isn't visible, the 3 above the 2 and 0 is visible. this is the flaw in the calculations.
